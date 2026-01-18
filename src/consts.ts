d6607643e5836f9b93776882b4c92f04b9c66c02
LeTeeno <jayson@deplex.org>
2026-01-18 11:28:48 +0100

    asfasf

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "lexy's site";
-export const SITE_DESCRIPTION = 'insert description here';
+export const SITE_TITLE = "rina's site";
+export const SITE_DESCRIPTION = 'retard in the world wide web';
